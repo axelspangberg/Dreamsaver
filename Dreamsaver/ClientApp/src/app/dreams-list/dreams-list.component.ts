000ae220ada25c9156d15bf54f409f6121e7ca34
Axel Spångberg <axspangberg@gmail.com>
2019-11-24 19:16:33 +0100

    Added dreams component to frontend and made some minor cleanup.

---
+++ Dreamsaver/ClientApp/src/app/dreams-list/dreams-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dreams-list',
+  templateUrl: './dreams-list.component.html',
+  styleUrls: ['./dreams-list.component.css']
+})
+export class DreamsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Dreamsaver/ClientApp/src/app/dreams-list/dreams-list.model.ts
@@ -0,0 +1,8 @@
+// example of a basic interface
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
